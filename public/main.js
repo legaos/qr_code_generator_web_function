312dd1bd4fce9306b8cf92fa456f57c2bb60f055
leonardosouzantt <legaos1711@gmail.com>
2026-03-03 22:19:33 -0300

    ...

--- public/main.js
+++ public/main.js
@@ -1,16 +1,19 @@
 async function gerar() {
   const texto = document.getElementById("txt").value;
-  if (!texto) return alert("Digite algo!");
+
+  if (!texto) {
+    alert("Digite algum texto!");
+    return;
+  }
 
   const resp = await fetch(`/api/gerar_qr?texto=${encodeURIComponent(texto)}`);
   const data = await resp.json();
 
   if (!data.ok) {
-    document.getElementById("resultado").innerHTML = `<p>Erro ao gerar!</p>`;
+    document.getElementById("resultado").innerHTML = "<p>Erro!</p>";
     return;
   }
 
-  // Exibir imagem base64
   document.getElementById("resultado").innerHTML =
-    `<img src="data:image/png;base64,${data.qr_base64}" />`;
+    `data:image/png;base64,${data.qr_base64}`;
 }
